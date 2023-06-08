10f9f82912e1ea7465738c1189fef34209331ce1
DevarajP2713 <devarajupm7@gmail,com>
2023-06-08 11:58:21 +0530

    First Push

---
+++ src/src/webparts/teamsAppDevelop/components/ITeamsAppDevelopProps.ts
@@ -0,0 +1,7 @@
+export interface ITeamsAppDevelopProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/src/webparts/teamsAppDevelop/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ITeamsAppDevelopWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'TeamsAppDevelopWebPartStrings' {
+  const strings: ITeamsAppDevelopWebPartStrings;
+  export = strings;
+}
